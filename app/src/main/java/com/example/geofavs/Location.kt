9afa589752fb0cf032cdb4659e0a23ca9d53ac91
Michele <miguelalfonsopardo@gmail.com>
2019-11-08 12:44:21 +0100

    branch dev created

--- app/src/main/java/com/example/geofavs/Location.kt
+++ app/src/main/java/com/example/geofavs/Location.kt
@@ -6,6 +6,7 @@ import androidx.room.PrimaryKey
 @Entity
 data class Location(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
     val longitude: Double,
     val latitude: Double
     )
